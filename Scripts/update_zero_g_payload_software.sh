94fac3903b4f1bf03f121c374f718826d1c422d1
eric-tapio <etapio@alumni.stanford.edu>
2025-04-10 13:04:13 -0700

    Add files via upload
    Adding SW updater sh file

---
+++ Scripts/update_zero_g_payload_software.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo " ~ Updating Zero G Payload software ..."
+echo ""
+
+cd ~/Zero_G_Payload
+git pull https://github.com/eric-tapio/Zero_G_Payload.git main
+
+echo ""
+echo " ~ Zero G Payload software update Complete!"
+echo ""
